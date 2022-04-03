c290c59f36851a2d7f90b8c962d20c98ae3756e9
Ilya Gerasimov <ilya.gerasimov.97@mail.ru>
2022-04-03 16:20:46 +0300

    прописали необходимые методы в LoginContract

---
+++ app/src/main/java/com/example/my_app_c5/LoginContract.kt
@@ -0,0 +1,18 @@
+package com.example.my_app_c5
+
+
+class LoginContract {
+    interface View{
+        fun setSuccess()
+        fun setError(error: String)
+        fun showProgress()
+        fun hideProgress()
+
+    }
+
+    interface Presenter{
+
+        fun onAttach(view: View)
+        fun onLogin(login: String, password:String)
+    }
+}
---
+++ app/src/main/java/com/example/my_app_c5/LoginPresenter.kt
@@ -0,0 +1,5 @@
+package com.example.my_app_c5
+
+class LoginPresenter{
+
+}
